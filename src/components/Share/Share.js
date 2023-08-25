ab08a5127806c006cc917a8206c46e02d0c93a2f
Smriti <smrtghoshal24@gmail.com>
2023-08-26 00:12:05 +0530

    chore:configure diffrent optionicon using material ui.

--- src/components/Share/Share.js
+++ src/components/Share/Share.js
@@ -1,5 +1,8 @@
 import "./share.css";
 import PermMediaIcon from "@mui/icons-material/PermMedia";
+import LabelIcon from "@mui/icons-material/Label";
+import RoomIcon from "@mui/icons-material/Room";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 
 export default function Share() {
   return (
@@ -20,8 +23,22 @@ export default function Share() {
         <div className="shareBottom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMediaIcon className="shreIcon" />
+              <PermMediaIcon className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
+            </div>
+
+            <div className="shareOption">
+              <LabelIcon className="shareIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+
+            <div className="shareOption">
+              <RoomIcon className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotionsIcon className="shareIcon" />
+              <span className="shareOptionText">Feelings</span>
             </div>
           </div>
         </div>
