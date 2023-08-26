9b84573d649a6026fc8203302f2b5712e5187075
Smriti <smrtghoshal24@gmail.com>
2023-08-26 23:09:41 +0530

    chore: add some emoji functionality using material icon.

--- src/components/Share/Share.js
+++ src/components/Share/Share.js
@@ -23,24 +23,25 @@ export default function Share() {
         <div className="shareBottom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMediaIcon className="shareIcon" />
+              <PermMediaIcon htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
             </div>
 
             <div className="shareOption">
-              <LabelIcon className="shareIcon" />
+              <LabelIcon htmlColor="blue" className="shareIcon" />
               <span className="shareOptionText">Tag</span>
             </div>
 
             <div className="shareOption">
-              <RoomIcon className="shareIcon" />
+              <RoomIcon htmlColor="green" className="shareIcon" />
               <span className="shareOptionText">Location</span>
             </div>
             <div className="shareOption">
-              <EmojiEmotionsIcon className="shareIcon" />
+              <EmojiEmotionsIcon htmlColor="goldenrod" className="shareIcon" />
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
+          <button className="shareButton">Share</button>
         </div>
       </div>
     </div>
