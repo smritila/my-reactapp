c29bb0edb9e9bd8bc5de23569321796ddfd729a4
Smriti <smrtghoshal24@gmail.com>
2023-08-30 12:47:15 +0530

    chore: Implement changes in Post.js.

---
+++ src/components/post/Post.js
@@ -0,0 +1,41 @@
+import "./post.css";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src="/assets/person/person1.jpg"
+              alt=""
+            />
+            <span className="postUsername">Safak Kocaoglu</span>
+
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVertIcon />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hey! its my first post:)</span>
+
+          <img className="postImg" src="assets/post/1.jpg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img className="likeIcon" src="assets/heart.png" alt="" />
+            <span className="postLikeCounter">32 people liked it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
